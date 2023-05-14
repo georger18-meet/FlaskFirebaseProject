b2ead0f4bfd09bb2280421143a77492fea15cb5c
George Rashed <goerge1110@gmail.com>
2023-05-15 02:48:07 +0300

    Login Logout.

--- FlaskFirebaseProject/main.py
+++ FlaskFirebaseProject/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, flash, jsonify, request, redirect
+from flask import session as login_session
 from forms import *
 import random
 import requests,json
@@ -11,11 +12,12 @@ firebaseConfig = {
   "storageBucket": "flaskfirebase-db196.appspot.com",
   "messagingSenderId": "588875081912",
   "appId": "1:588875081912:web:b88f8965a8f84b62f52c4d",
-  "measurementId": "G-H2MQD00P28"
+  "measurementId": "G-H2MQD00P28",
+  "databaseURL": ""
 }
 
-# firebase = pyrebase.initialize_app(firebaseConfig)
-# auth = firebase.auth()
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
 
 
 app = Flask(  # Create a flask app
@@ -31,37 +33,61 @@ formpy = ValidationForms("","","","")
 @app.route('/')  # '/' for the default page
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', login_session = login_session)
         
 @app.route('/about')
 def about():
-    return render_template('about.html', title = "About")
+    return render_template('about.html', login_session = login_session, title = "About")
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-    if request.method == 'GET':
-        return render_template('login.html', title = "Log In")
-    else:
-        if formpy.ValidateLogIn(request.form.get("email"),request.form.get("psw")):
-            flash(f'Loged In Successfuly!', 'success')
-            return redirect(url_for('home')) 
+    if login_session["name"] != None:
+        return redirect(url_for('home'))
+    else:        
+        if request.method == 'GET':
+            return render_template('login.html', login_session = login_session, title = "Log In")
         else:
-            flash(f'Check Your Credintials! Invalid Email or Password.', 'danger')
-            return render_template('login.html', title = "Log In")
+            try:
+                login_session["name"] = auth.sign_in_with_email_and_password(request.form.get("email"),request.form.get("psw"))
+                flash(f'Loged In Successfuly!', 'success')
+                return redirect(url_for('home'))
+            except:
+                flash(f'Check Your Credintials! Invalid Email or Password.', 'danger')
+                return render_template('login.html', login_session = login_session, title = "Log In")
+            # if formpy.ValidateLogIn(request.form.get("email"),request.form.get("psw")):
+                # flash(f'Loged In Successfuly!', 'success')
+                # return redirect(url_for('home')) 
+            # else:
+                # flash(f'Check Your Credintials! Invalid Email or Password.', 'danger')
+                # return render_template('login.html', title = "Log In")
     
 @app.route('/signup', methods=['GET','POST'])
 def signup():
-    if request.method == 'GET':
-        return render_template('signup.html', title = "Sign Up")
+    if login_session["name"] != None:
+        return redirect(url_for('home'))
     else:
-        if formpy.ValidateSignUp(request.form.get("username"),request.form.get("email"),request.form.get("psw"),request.form.get("confirm-psw")):
-            flash(f'Account created for { request.form.get("username") }!', 'success')
-            return redirect(url_for('home'))
+        if request.method == 'GET':
+            return render_template('signup.html', login_session = login_session, title = "Sign Up")
         else:
-            flash(f'Check Your Credintials! Invalid Username, Email or Password.', 'danger')
-            return render_template('signup.html', title = "Sign Up")
+            try:
+                login_session["name"] = auth.create_user_with_email_and_password(request.form.get("email"),request.form.get("psw"))
+                flash(f'Account created for { request.form.get("username") }!', 'success')
+                return redirect(url_for('home'))
+            except:
+                flash(f'Check Your Credintials! Invalid Username, Email or Password.', 'danger')
+                return render_template('signup.html', login_session = login_session, title = "Sign Up")
+            # if formpy.ValidateSignUp(request.form.get("username"),request.form.get("email"),request.form.get("psw"),request.form.get("confirm-psw")):
+                # flash(f'Account created for { request.form.get("username") }!', 'success')
+                # return redirect(url_for('home'))
+            # else:
+                # flash(f'Check Your Credintials! Invalid Username, Email or Password.', 'danger')
+                # return render_template('signup.html', title = "Sign Up")
 
-
+@app.route('/logout')
+def logout():
+    login_session["name"] = None
+    auth.current_user = None
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":  # Makes sure this is the main process
     app.run( # Starts the site
